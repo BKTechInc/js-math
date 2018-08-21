8a89290f53c535822701f55aa5cd92146aa28257
RocketBear27 <rocketbear27.code@gmail.com>
2018-08-21 07:34:02 -0700

    Configuring Addition

---
+++ script.js
@@ -0,0 +1,14 @@
+function add() {
+    // Getting the numbers put in by the user
+    var num1 = document.getElementById('add').innerHTML;
+    var num2 = document.getElementById('add2').innerHTML;
+    // Convert input strings into numbers
+    var n = Number(num1) + Number(num1);
+    // Checks to see if any of the boxes are empty, if so display error alert, if not, console.log a message and display answer
+    if (num1 || num2 === null) {
+        alert('Error 100: Uncompleted equation');
+    } else {
+        console.log('Adding numbers together....');
+        document.getElementById('addAnswer').innerHTML = n;
+    }
+};
