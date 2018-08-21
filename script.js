782104213f3c25b4ab21c227ed270152d6c7c1e7
RocketBear27 <rocketbear27.code@gmail.com>
2018-08-21 09:18:39 -0700

    Working on a bug for adding

--- script.js
+++ script.js
@@ -3,13 +3,13 @@ function add() {
     var num1 = document.getElementById('add').innerHTML;
     var num2 = document.getElementById('add2').innerHTML;
     // Convert input strings into numbers
-    var n1 = Number(num1);
-    var n2 = Number(num2);
+    var numbr = Number(num1 + num2);
     // Checks to see if any of the boxes are empty, if so display error alert, if not, console.log a message and display answer
-    if (num1 || num2 === null) {
-        alert('Error 100: Uncompleted equation');
+    if (numbr === null) {
+        alert('Error: Uncompleted equation');
     } else {
         console.log('Adding numbers together....');
-        document.getElementById('addAnswer').innerHTML = n1 + n2;
+        document.getElementById('addAnswer').innerHTML = numbr;
+        console.log(numbr);
     }
 };
