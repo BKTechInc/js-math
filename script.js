bcf8f9c1b198c74d0cc69d29de700853cca49abe
RocketBear27 <rocketbear27.code@gmail.com>
2018-08-21 18:26:07 -0700

    Reused code with .selectedIndex

--- script.js
+++ script.js
@@ -1,15 +1,13 @@
 function add() {
-    // Getting the numbers put in by the user
-    var num1 = document.getElementById('add').innerHTML;
-    var num2 = document.getElementById('add2').innerHTML;
-    // Convert input strings into numbers
-    var numbr = Number(num1 + num2);
-    // Checks to see if any of the boxes are empty, if so display error alert, if not, console.log a message and display answer
-    if (numbr === null) {
+    var num1 = document.getElementById('add').selectedIndex;
+    var num2 = document.getElementById('add2').selectedIndex;
+    var input = document.getElementsByClassName('additionBox');
+    var numbr = Number(num1);
+    var numbr2 = Number(num2);
+    var answer = numbr + numbr2;
+    if (input === null) {
         alert('Error: Uncompleted equation');
     } else {
-        console.log('Adding numbers together....');
-        document.getElementById('addAnswer').innerHTML = numbr;
-        console.log(numbr);
+        document.getElementById('addAnswer').innerHTML = answer.value;
     }
 };
