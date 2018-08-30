8ca0d73d8afba777bfdd8cce0600c47e6f80cdc1
RocketBear27 <rocketbear27.code@gmail.com>
2018-08-29 17:00:15 -0700

    Changed id names

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 function add() {
-    var num1 = document.getElementById('add');
-    var num2 = document.getElementById('add2');
+    var num1 = document.getElementById('addNum1');
+    var num2 = document.getElementById('addNum2');
     var numbr = Number(num1);
     var numbr2 = Number(num2);
     alert(numbr + numbr2);
