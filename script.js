e01c15ac86fa2312a967bc5bb6740d2865cd42d8
RocketBear27 <rocketbear27.code@gmail.com>
2018-09-01 14:47:41 -0700

    Algorithms WORKING!!

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 function add() {
-    var num1 = document.getElementById('addNum1');
-    var num2 = document.getElementById('addNum2');
+    var num1 = document.getElementById('addNum1').value;
+    var num2 = document.getElementById('addNum2').value;
     var numbr = Number(num1);
     var numbr2 = Number(num2);
     var answer = numbr + numbr2;
