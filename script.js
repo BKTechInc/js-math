c7bcc653f1ebb1bb9b55ee869f8dd57b54903db2
RocketBear27 <rocketbear27.code@gmail.com>
2018-09-01 15:22:29 -0700

    Added subtraction section

--- script.js
+++ script.js
@@ -12,3 +12,17 @@ function add() {
     // Displaying answer in answer box
     document.getElementById('addAnswer').innerHTML = answer;
 }
+function sub() {
+    // Retrieving the text value of the input 
+    var num1 = document.getElementById('subNum1').value;
+    var num2 = document.getElementById('subNum2').value;
+    // Converting the text input to a number
+    var numbr = Number(num1);
+    var numbr2 = Number(num2);
+    // Subtract numbers together
+    var answer = numbr - numbr2;
+    // Logging answer
+    console.log(answer);
+    // Displaying answer in answer box
+    document.getElementById('subAnswer').innerHTML = answer;
+}
